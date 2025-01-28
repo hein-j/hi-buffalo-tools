1111f13a1f30ee86cd51f72a7df8f2434c97c0c0
Hein Jeong <hein.jeong@gmail.com>
2025-01-28 14:26:44 -0500

    Build home page & add get addresses for paper mail functionality

---
+++ src/components/get-addresses-for-paper-mail/getPaperMailCustomerList.ts
@@ -0,0 +1,80 @@
+import { parse } from "csv-parse/browser/esm/sync";
+import { stringify } from "csv-stringify/browser/esm/sync";
+
+export function getJSONFromCSV(csv: string, objname?: string) {
+  csv = csv.replace(/^\uFEFF/, ""); // strip BOM markers
+
+  return parse(csv, {
+    columns: true,
+    objname,
+  });
+}
+
+export function getCSVFromJSON(pathName: string, records: any[]) {
+  return stringify(records, { header: true });
+}
+
+export function getPaperMailCustomerList(fieldEntriesCSV: string) {
+  function getValueFromInfo(info: any, key: string) {
+    return info[key]?.value ?? "";
+  }
+  const fieldEntriesJSON = getJSONFromCSV(fieldEntriesCSV);
+
+  const customers = fieldEntriesJSON.reduce(
+    (acc: any, curr: any) => {
+      const fieldName = curr["Field name"];
+
+      let next = { value: curr["Field value"], date: curr["Date"] };
+      const prev = acc[curr.Email]?.[fieldName];
+      // if prev is more current, keep prev
+      if (prev && prev.date > next.date) {
+        next = prev;
+      }
+
+      return {
+        ...acc,
+        [curr["Email"]]: {
+          ...(acc[curr.Email] ?? {
+            "First name": curr["First name"],
+            "Last name": curr["Last name"],
+          }),
+          [fieldName]: next,
+        },
+      };
+    },
+    {} as {
+      [email: string]: {
+        [fieldName: string]: { value: string; date: string } | string;
+      };
+    }
+  );
+
+  const customersOptingPaper = Object.entries(customers).reduce(
+    (acc, [email, info]: [string, any]) => {
+      // sometimes, email is ''
+      if (!email) {
+        return acc;
+      }
+      if (getValueFromInfo(info, "Paper Mail") !== "Yes") {
+        return acc;
+      }
+
+      acc.push({
+        Email: email,
+        "First name": info["First name"],
+        "Last name": info["Last name"],
+        "Street Address": getValueFromInfo(info, "Street Address"),
+        City: getValueFromInfo(info, "City"),
+        State: getValueFromInfo(info, "State"),
+        "Zip Code": getValueFromInfo(info, "Zip Code"),
+      });
+
+      return acc;
+    },
+    [] as any
+  );
+
+  console.log(JSON.stringify(customersOptingPaper));
+
+  return getCSVFromJSON("addresses", customersOptingPaper);
+}
---
+++ src/components/get-addresses-for-paper-mail/index.tsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+import { useState } from "react";
+import { getPaperMailCustomerList } from "./getPaperMailCustomerList";
+
+export const GetAddressesForPaperMail = () => {
+  const [downloadLinkSettings, setDownloadLinkSettings] = useState<{
+    show: boolean;
+    href: string | undefined;
+    download: string | undefined;
+  }>({
+    show: false,
+    href: undefined,
+    download: undefined,
+  });
+  const [inputIsDisabled, setInputIsDisabled] = useState(false);
+  const [statusText, setStatusText] = useState("");
+
+  return (
+    <div>
+      <h2>Get Addresses for Paper Mail</h2>
+      <ol>
+        <li>Go to Momence</li>
+        <li>From the sidebar, click into "Analytics" &gt; "Reports"</li>
+        <li>
+          Scroll and find the section "Customers." Click into "Custom fields"
+        </li>
+        <li>Click "Download summary" in the top-left corner.</li>
+        <li>Upload the downloaded file here.</li>
+      </ol>
+      <label htmlFor="momence-csv">Upload CSV:</label>
+      <input
+        disabled={inputIsDisabled}
+        type="file"
+        id="momence-csv"
+        name="momence-csv"
+        accept=".csv"
+        onChange={(e) => {
+          console.log("change");
+          console.log("FILES", e.target.files);
+          const file = e.target.files?.[0];
+          if (file) {
+            setInputIsDisabled(true);
+            setStatusText("Working...");
+            const reader = new FileReader();
+            reader.readAsText(file);
+            reader.onload = () => {
+              try {
+                if (reader.result) {
+                  if (typeof reader.result !== "string") {
+                    throw new Error("Reader read file as Array Buffer!");
+                  }
+                  const csvContent = getPaperMailCustomerList(reader.result);
+                  console.log(csvContent);
+                  const fileName = "customers-opting-paper.csv";
+                  const csvFile = new File([csvContent], fileName, {
+                    type: "text/plain",
+                  });
+                  setDownloadLinkSettings({
+                    show: true,
+                    href: URL.createObjectURL(csvFile),
+                    download: fileName,
+                  });
+
+                  setStatusText("Success!");
+                }
+              } catch (e) {
+                setStatusText("Error: " + e);
+              }
+            };
+            reader.onerror = () => {
+              console.log(reader.error);
+              setStatusText("Error: " + e);
+            };
+          }
+        }}
+      />
+      <p>{statusText}</p>
+      {downloadLinkSettings.show && (
+        <a
+          href={downloadLinkSettings.href}
+          download={downloadLinkSettings.download}
+        >
+          Download file
+        </a>
+      )}
+    </div>
+  );
+};
